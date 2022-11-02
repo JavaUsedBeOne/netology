686083c140371c86469c663432ec3015e9eb8761
JavaUsedBeOne <kisarevna@yandex.ru>
2022-11-02 10:54:44 +0800

    Change SalesManager

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {1, 44, 788, 90};
+        long[] sales = {1, 44, 788, 90};
         SalesManager manager = new SalesManager(sales);
         System.out.println("Макс  " + manager.max());
         }
