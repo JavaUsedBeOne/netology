b6bfc7be9bca8a1eede70a9bab326365bbf0ba86
JavaUsedBeOne <kisarevna@yandex.ru>
2022-11-02 10:37:14 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = {1, 44, 788, 90};
+        SalesManager manager = new SalesManager(sales);
+        System.out.println("Макс  " + manager.max());
+        }
+
+    }
+
